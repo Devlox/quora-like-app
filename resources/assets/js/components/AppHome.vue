13b2d951ddec8d257cfd67379e7069ff4860e1b9
John M <muyambojohn1@gmail.com>
2019-06-03 14:06:39 +0200

    Vuitify, Vue Router and initial components

---
+++ resources/assets/js/components/AppHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import toolbar from './Toolbar'
+import AppFooter from './AppFooter';
+export default {
+    components:{toolbar, AppFooter}     
+}
+</script>
+
+<style>
+
+</style>
